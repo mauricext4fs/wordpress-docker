62a7ee0a64d736495efb1108d2c7d048f0859211
Maurice Courtois <mcourtois@centos8.cig.baar>
2019-10-16 04:38:18 +0200

    - Finalize the mail configuration script - Move ssmtp.conf as hidden file because it contain password

--- gen_ssmtp.conf.sh
+++ gen_ssmtp.conf.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-if [[ $SSMTP_TO ]] && [[ $SSMTP_USER ]] && [[ $SSMTP_PASS ]]; then
 
-# set reasonable defaults
+echo
+echo
+echo "Let's configure sending Email with Wordpress"
+echo 
+echo 
 
-SSMTP_TLS=${SSMTP_TLS:-YES}
-SSMTP_SERVER=${SSMTP_SERVER:-smtp.gmail.com}
-SSMTP_PORT=${SSMTP_PORT:-587}
-SSMTP_HOSTNAME=${SSMTP_HOSTNAME:-localhost}
-SSMTP_FROM=${SSMTP_FROM:-USERNAME@HOSTNAME}
+read -p "Mail server Hostname (gmail smtp.gmail.com) : "
+SSMTP_SERVER=$REPLY
+read -p "Mail Server port (gmail 587) : "
+SSMTP_PORT=$REPLY
+read -p "Username : "
+SSMTP_USER=$REPLY
+read -sp "Pass : "
+SSMTP_PASS=$REPLY
+echo " "
+read -p "Tls? (YES or NO) : "
+SSMTP_TLS=$REPLY
+SSMTP_HOSTNAME=`curl ifconfig.co`
 
-# root=$SSMTP_TO
-cat << EOF > ssmtp.conf
+cat <<EOF >.ssmtp.conf
 mailhub=$SSMTP_SERVER:$SSMTP_PORT
 AuthUser=$SSMTP_USER
 AuthPass=$SSMTP_PASS
